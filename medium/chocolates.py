84a241f0f13fbb9a5e98de73f6591201447c5551
fabiohj7 <fabiohj7@gmail.com>
2024-08-04 02:16:51 -0400

    chocolates update

--- medium/chocolates.py
+++ medium/chocolates.py
@@ -10,22 +10,48 @@ def main():
         i += 1
         binary = f'{i:b}'.zfill(n)
         matrix = [binary[j:j + c] for j in range(0, r * c, c)]
-        for item in matrix:
-            print(item)
+        # print()
+        # for item in matrix:
+        #     print(item)
 
         ones = []
+        ceros = []
         for x in range(r):
             for y in range(c):
                 if matrix[x][y] == '1':
                     ones.append((x, y))
+                else:
+                    ceros.append((x, y))
         connected = check_around(ones, matrix, r, c)
-        print("Connected: ", connected)
-        print("Ones: ", ones)
+        # print("Connected: ", connected)
+        # print("Ones: ", ones)
+        hole = False
         if len(connected) == len(ones):
-            print("MATCH!")
+            if ceros:
+                hole = True
+                hole = check_holes(ceros, matrix, r, c)
+                if hole:
+                    continue
             ans += 1
 
     print(ans)
+
+
+def check_holes(ceros, matrix, r, c):
+    to_explore = deque([ceros[0]])
+    explored = [ceros[0]]
+    directions = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1),
+                  (1, -1)]
+    while to_explore:
+        x, y = to_explore.popleft()
+        if 0 < x < r - 1 and 0 < y < c - 1:
+            for dx, dy in directions:
+                nx, ny = x + dx, y + dy
+                if (0 >= nx >= r - 1
+                        or 0 >= ny >= c - 1) and matrix[nx][ny] == '0':
+                    explored.append((nx, ny))
+                    to_explore.append((nx, ny))
+                    return False
 
 
 def check_around(ones, matrix, r, c):
